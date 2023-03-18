11cccccb98983c536cadbcf36262909507b9e463
aaqib <aaqibjavaid605@gmail.com>
2023-03-18 07:57:45 +0530

    implement smooth scrolling

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const header = document.querySelector(".header");
 const nav = document.querySelector(".nav");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const allSections = document.querySelectorAll(".section");
+const allButtons = document.getElementsByTagName("button");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,97 +36,6 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// Observer callback
-const stickyNav = function (entries) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) nav.classList.add("sticky");
-  else nav.classList.remove("sticky");
-};
-
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0,
-  rootMargin: "-90px",
-});
-headerObserver.observe(header); //Observing the target element (header)
-
-// Reveal sections
-
-// const allSections = document.querySelectorAll('.section');
-
-// const revealSection = function (entries, observer) {
-//   const [entry] = entries;
-
-//   if (!entry.isIntersecting) return;
-//   entry.target.classList.remove('section--hidden');
-//   observer.unobserve(entry.target);
-// };
-
-// const sectionObserver = new IntersectionObserver(revealSection, {
-//   root: null,
-//   threshold: 0.15,
-// });
-
-// allSections.forEach(section => {
-//   sectionObserver.observe(section);
-//   section.classList.add('section--hidden');
-// });
-
-const allSections = document.querySelectorAll(".section");
-const allButtons = document.getElementsByTagName("button");
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-
-function revealSection(entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target);
-}
-
-allSections.forEach((section) => {
-  sectionObserver.observe(section);
-  // section.classList.add('section--hidden');
-});
-
-// Lazy loading images
-const imgTargets = document.querySelectorAll("img[data-src]");
-
-const loadImg = (entries, observer) => {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  // Replace src with data-src
-  entry.target.src = entry.target.dataset.src;
-  entry.target.classList.remove("lazy-img");
-};
-
-const imgObserver = new IntersectionObserver(loadImg, {
-  root: null,
-  threshold: 0,
-});
-
-imgTargets.forEach((img) => imgObserver.observe(img));
-
-// Building slider
-
-const slider = document.querySelector(".slider");
-slider.style.transform = `scale(.4)`;
-slider.style.overflow = `visible`;
-
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
-
-// window.addEventListener('beforeunload', e => {
-//   e.preventDefault();
-//   e.returnValue = '';
-// });
-
 // Create and insert the cookie message
 const cookieEl = document.createElement("div");
 cookieEl.classList.add("cookie-message");
@@ -140,3 +53,27 @@ btnCloseCookieMessage.addEventListener("click", function () {
 
 cookieEl.style.height =
   parseFloat(getComputedStyle(cookieEl).height) + 50 + "px";
+
+// Smooth scrolling
+btnScrollTo.addEventListener("click", function (e) {
+  const s1Coords = section1.getBoundingClientRect();
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log(window.scrollX, window.scrollY);
+
+  console
+    .log
+    // document.documentElement.clientHeight,
+    // document.documentElement.clientWidth
+    ();
+
+  // console.log(s1Coords.left, s1Coords.top);
+
+  // window.scrollTo({
+  //   left: s1Coords.left + window.scrollX,
+  //   top: s1Coords.top + window.scrollY,
+  //   behavior: "smooth",
+  // });
+
+  // section1.scrollIntoView({ behavior: "smooth" });
+});
