4f5ca7cebef4e13560d6f36b036479e7757eb8b4
aaqib <aaqibjavaid605@gmail.com>
2023-03-23 07:59:59 +0530

    add fade animation in navbar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 ///////////////////////////////////////
-// Modal window
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
@@ -13,6 +12,9 @@ const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 const allSections = document.querySelectorAll(".section");
 const allButtons = document.getElementsByTagName("button");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -90,9 +92,6 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 });
 
 // Tabbed component
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
@@ -115,3 +114,22 @@ tabsContainer.addEventListener("click", function (e) {
   );
   activeContentArea.classList.add("operations__content--active");
 });
+
+// Menu fade animation
+const fadeAnimation = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((sibling) => {
+      if (sibling !== link) {
+        sibling.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", fadeAnimation.bind(0.5));
+nav.addEventListener("mouseout", fadeAnimation.bind(1));
